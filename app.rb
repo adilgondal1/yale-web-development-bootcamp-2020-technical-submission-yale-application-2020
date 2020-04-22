241f0a4e6004020799960bcbd7a25652d94f8b10
Adil Gondal <adil.gondal@yale.edu>
2020-04-22 05:50:46 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,6 +2,11 @@ require_relative './test'
 AVERAGE_HEIGHT_IN_INCHES = 67
 
 # Your Code Here!
+name = "Adil"
+age = 18
+favorite_food = "pizza"
+height_in_inches = 70
+height_message = height_in_inches > AVERAGE_HEIGHT_IN_INCHES ? 
 
 ## Do not modify below this line!
 
